35cc291fb87b175d1486f85cbdbff800ca896dc2
Hema Sai Krishna Kadiyala <hemasaikrishna@gmail.com>
2017-07-23 05:28:35 +0000

    all names changed

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ router.post("/register",function(req,res){
             return res.redirect("/register");
         }
         passport.authenticate("local")(req,res,function(){
-           req.flash("success","Welcome to Campground Finder"+user.username);
+           req.flash("success","Welcome to Campground Finder "+user.username);
            res.redirect("/campgrounds");
         });
     });
