52f901607bb0cd30fdeb75ea4171461056968038
Hema Sai Krishna Kadiyala <hemasaikrishna@gmail.com>
2017-07-26 02:16:17 +0000

    add google maps

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,7 @@ var express=require("express");
 var router=express.Router({mergeParams: true});
 var Campground=require("../models/campground");
 var middleware=require("../middleware");
+var geocoder=require("geocoder");
 
 //index
 router.get("/",function(req,res){
@@ -40,15 +41,25 @@ router.post("/",middleware.isLoggedIn , function(req,res){
         id: req.user._id,
         username: req.user.username
     };
-    var newCampground= {name: name,price: price,image: image , description : desc, author: author};
-    Campground.create(newCampground,function(err,newlyCreated){
+    geocoder.geocode(req.body.location,function(err,data){
+        if(err){
+            console.log(err);
+            req.flash("error","something went wrong");
+            res.redirect("back");
+        } else {
+        var lat=data.results[0].geometry.location.lat;
+        var lng=data.results[0].geometry.location.lng;
+        var location = data.results[0].formatted_address;
+        var newCampground = {name: name, image: image, description: desc, price: price, author:author, location: location, lat: lat, lng: lng};
+        
+        Campground.create(newCampground,function(err,newlyCreated){
         if(err){
             console.log(err);
         }else{
             res.redirect("/campgrounds");
         }
+    }); }
     });
-    
 });
 
 //Edit Route
@@ -64,7 +75,7 @@ router.get("/:id/edit",middleware.checkCampgroundOwnership ,function(req,res){
 });
 
 //Update Route
-router.put("/:id",middleware.checkCampgroundOwnership, function(req,res){
+/*router.put("/:id",middleware.checkCampgroundOwnership, function(req,res){
     Campground.findByIdAndUpdate(req.params.id,req.body.campground,function(err,campground){
         if(err){
             res.redirect("/campgrounds");
@@ -72,6 +83,29 @@ router.put("/:id",middleware.checkCampgroundOwnership, function(req,res){
             res.redirect("/campgrounds/"+req.params.id);
         }
     });
+});*/
+
+router.put("/:id", function(req, res){
+  geocoder.geocode(req.body.location, function (err, data) {
+    if(err){
+            console.log(err);
+            req.flash("error","something went wrong");
+            res.redirect("back");
+        } else{
+    var lat = data.results[0].geometry.location.lat;
+    var lng = data.results[0].geometry.location.lng;
+    var location = data.results[0].formatted_address;
+    var newData = {name: req.body.name, image: req.body.image, description: req.body.description, price: req.body.price, location: location, lat: lat, lng: lng};
+    Campground.findByIdAndUpdate(req.params.id, {$set: newData}, function(err, campground){
+        if(err){
+            req.flash("error", err.message);
+            res.redirect("back");
+        } else {
+            req.flash("success","Successfully Updated!");
+            res.redirect("/campgrounds/" + campground._id);
+        }
+    }); }
+  });
 });
 
 //Delete Route
