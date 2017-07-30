97152d88985e6f417733e5ace0ede4a31be2d19d
Hema Sai Krishna Kadiyala <hemasaikrishna@gmail.com>
2017-07-30 03:36:27 +0000

    update nav bar

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get("/",function(req,res){
 //AUTH Routes
 
 router.get("/register",function(req,res){
-    res.render("register");
+    res.render("register",{page:'render'});
 });
 
 router.post("/register",function(req,res){
@@ -32,7 +32,7 @@ router.post("/register",function(req,res){
 });
 
 router.get("/login",function(req,res){
-    res.render("login");
+    res.render("login",{page: 'login'});
 });
 
 router.post("/login",passport.authenticate("local",{
